aefbd17ba6b347d865b09cb4b6a275e88819e690
Kasbolat Kumakhov <uhfath@gmail.com>
2017-02-28 20:39:30 +0300

    Added abilit to choose between getDecor and layout views used for animation

--- app/src/main/java/com/company/user/circularrevealtest/MainActivity.java
+++ app/src/main/java/com/company/user/circularrevealtest/MainActivity.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.ViewOutlineProvider;
 import android.view.ViewTreeObserver;
+import android.widget.CheckBox;
 
 import java.util.Random;
 
@@ -23,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         return (float) Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
     }
 
-    private static Animator getRevealAnimator(Activity activity, int x, int y, long duration) {
+    private static Animator getRevealAnimator(Activity activity, View view, int x, int y, long duration) {
         Display display = activity.getWindowManager().getDefaultDisplay();
 
         Point size = new Point();
@@ -35,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
         float d4 = distance(x, y, size.x, size.y);
         float radius = Math.max(Math.max(Math.max(d1, d2), d3), d4);
 
-        View view = activity.getWindow().getDecorView();
         Animator animator = (Animator) view.getTag();
         if (animator != null) {
             animator.cancel();
@@ -48,45 +48,13 @@ public class MainActivity extends AppCompatActivity {
         return animator;
     }
 
-    private static void startRevealAnimation(final Activity activity, final int x, final int y) {
-        final View view = activity.getWindow().getDecorView();
-        view.setOutlineProvider(new ViewOutlineProvider() {
-            @Override
-            public void getOutline(View view, Outline outline) {
-                outline.setOval(-1, -1, 0, 0);
-            }
-        });
-        view.setClipToOutline(true);
-
+    private static void startRevealAnimation(Activity activity, View view, int x, int y) {
         Random random = new Random();
         int color = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256));
         view.setBackgroundColor(color);
 
-        view.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
-            @Override
-            public boolean onPreDraw() {
-                ViewTreeObserver vto = view.getViewTreeObserver();
-                if (vto.isAlive()) {
-                    vto.removeOnPreDrawListener(this);
-                }
-                Animator animator = getRevealAnimator(activity, x, y, 1000);
-                animator.addListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        view.setClipToOutline(false);
-                    }
-
-                    @Override
-                    public void onAnimationCancel(Animator animation) {
-                        view.setClipToOutline(false);
-                    }
-                });
-                animator.start();
-
-                return false;
-            }
-        });
-
+        Animator animator = getRevealAnimator(activity, view, x, y, 1000);
+        animator.start();
     }
 
     @Override
@@ -94,11 +62,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View mainView = findViewById(R.id.activity_main);
+        final CheckBox checkBox = (CheckBox) findViewById(R.id.check_box);
+        final View mainView = findViewById(R.id.activity_main);
         mainView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                startRevealAnimation(MainActivity.this, (int) event.getRawX(), (int) event.getRawY());
+                startRevealAnimation(MainActivity.this, checkBox.isChecked() ? getWindow().getDecorView() : mainView, (int) event.getRawX(), (int) event.getRawY());
                 return false;
             }
         });
